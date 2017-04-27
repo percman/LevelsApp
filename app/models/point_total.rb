a1c9d7cc80e0ab2d8504f0362f1880ca8f20dda1
dabramson <dabram6@students.towson.edu>
2017-04-27 17:09:26 -0400

    Improved point total validations

--- app/models/point_total.rb
+++ app/models/point_total.rb
@@ -1,6 +1,7 @@
 class PointTotal < ApplicationRecord
   belongs_to :student
-  validates :score, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 100}, allow_nil: true
+  validates :score, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 100}, unless: :absent?
+  validates :score, presence: true, unless: :absent?
   validates :date, uniqueness: {:scope=> :student_id}
   validates :date, date: {before_or_equal_to: Proc.new{Date.today}}, on: :create
   validates :date, date: true
@@ -8,8 +9,6 @@ class PointTotal < ApplicationRecord
   def successful
     if absent?
       return nil
-    elsif score==nil
-      self.score=0
     end
     if (student.level=='1' && score>=70) || ((student.level=='2'||student.level=='OR')&&score>=80)||(student.level=='3'&& score>=85)
       return "yes"
